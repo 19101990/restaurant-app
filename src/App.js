6a1b09eebd9ba0f456dd36d19eb7106888f904ec
Nitin Gangwar <54510381+Nitin523@users.noreply.github.com>
2022-11-12 15:05:42 +0530

    Revert "Footer (#5)"
    This reverts commit d631ec4bb25ec9068ecf47c808cad722523039de.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-
 import "./App.css";
 import { Header } from "./components";
-import Footer from "./components/Footer"
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Footer />
     </div>
   );
 }
