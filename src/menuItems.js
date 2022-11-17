fb1c093235fd6cc30b62e28fb327b7dcaacf5574
Ewa Hostynska <38616184+19101990@users.noreply.github.com>
2022-11-17 22:11:03 +0100

    update items data

--- src/menuItems.js
+++ src/menuItems.js
@@ -1,58 +1,93 @@
 export const menuItems = [
-    {
-        id: 1,
-        title: "Margherita",
-        image: "https://images.unsplash.com/photo-1574071318508-1cdbab80d002?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80",
-        description: "tomato sauce, mozzarella, oregano",
-        category: "pizza"
-    },
-    {
-        id: 2,
-        title: "Vegetariana",
-        image: "https://images.unsplash.com/photo-1615595190084-c048920f5b03?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
-        description: "mozzarella, grilled broccoli, onion, garlic",
-        category: "pizza"
-    },
-    {
-        id: 3,
-        title: "BBQ Chicken",
-        image: "https://images.unsplash.com/photo-1523982440192-e61efd063acb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1547&q=80",
-        description: "BBQ sauce, mozzarella, fried chicken, onion, bell pepper",
-        category: "pizza"
-    },
-    {
-        id: 4,
-        title: "Rustica",
-        image: "https://images.unsplash.com/photo-1620374645366-9e2aea59e4c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-        description: "mozarella, fresh tomatoes, olives with stone, onions, oregano",
-        category: "pizza"
-    },
-    {
-        id: 5,
-        title: "Braccio di Ferro",
-        image: "https://images.unsplash.com/photo-1620374643809-b69c702d0ed4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-        description: "mozarella, spinach, tomatoes, olives, egg, garlic",
-        category: "pizza"
-    },
-    {
-        id: 6,
-        title: "Pepperoni",
-        image: "https://images.unsplash.com/photo-1605478371310-a9f1e96b4ff4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-        description: "mozarella, pepperoni, basil, garlic",
-        category: "pizza"
-    },
-    {
-        id: 7,
-        title: "Prosciutto",
-        image: "https://images.unsplash.com/photo-1536831410883-c715a6a92ed7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-        description: "mozarella, prosciutto crudo",
-        category: "pizza"
-    },
-    {
-        id: 8,
-        title: "Rucola",
-        image: "https://images.unsplash.com/photo-1528137871618-79d2761e3fd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-        description: "mozzarella, rocket, Grana Padano DOP, garlic",
-        category: "pizza"
-    },
+  {
+    id: 1,
+    title: "Margherita",
+    image: "https://images.unsplash.com/photo-1574071318508-1cdbab80d002?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80",
+    description: "tomato sauce, mozzarella, oregano",
+    category: "pizza"
+  },
+  {
+    id: 2,
+    title: "Vegetariana",
+    image: "https://images.unsplash.com/photo-1615595190084-c048920f5b03?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
+    description: "mozzarella, grilled broccoli, onion, garlic",
+    category: "pizza"
+  },
+  {
+    id: 3,
+    title: "BBQ Chicken",
+    image: "https://images.unsplash.com/photo-1523982440192-e61efd063acb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1547&q=80",
+    description: "BBQ sauce, mozzarella, fried chicken, onion, bell pepper",
+    category: "pizza"
+  },
+  {
+    id: 4,
+    title: "Rustica",
+    image: "https://images.unsplash.com/photo-1620374645366-9e2aea59e4c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    description: "mozarella, fresh tomatoes, olives with stone, onions, oregano",
+    category: "pizza"
+  },
+  {
+    id: 5,
+    title: "Braccio di Ferro",
+    image: "https://images.unsplash.com/photo-1620374643809-b69c702d0ed4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    description: "mozarella, spinach, tomatoes, olives, egg, garlic",
+    category: "pizza"
+  },
+  {
+    id: 6,
+    title: "Pepperoni",
+    image: "https://images.unsplash.com/photo-1605478371310-a9f1e96b4ff4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    description: "mozarella, pepperoni, basil, garlic",
+    category: "pizza"
+  },
+  {
+    id: 7,
+    title: "Prosciutto",
+    image: "https://images.unsplash.com/photo-1536831410883-c715a6a92ed7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    description: "mozarella, prosciutto crudo",
+    category: "pizza"
+  },
+  {
+    id: 8,
+    title: "Rucola",
+    image: "https://images.unsplash.com/photo-1528137871618-79d2761e3fd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    description: "mozzarella, rocket, Grana Padano DOP, garlic",
+    category: "pizza"
+  },
+  {
+    id: 9,
+    title: "Hamburger",
+    image: "https://images.unsplash.com/photo-1576843776838-032ac46fbb93?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1430&q=80",
+    description: "tomato, lettuce, onions, grindhouse sauce",
+    category: "burger"
+  },
+  {
+    id: 10,
+    title: "Cheeseburger",
+    image: "https://images.unsplash.com/photo-1534790566855-4cb788d389ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    description: "tomato, lettuce, onions, cheddar, grindhouse sauce",
+    category: "burger"
+  },
+  {
+    id: 11,
+    title: "Crispy Chickenburger",
+    image: "https://images.unsplash.com/photo-1544120379-4428412e6568?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    description: "portobello Mushroom, Grilled Pineapple, Soy-Hoisin Glaze, Cheddar",
+    category: "burger"
+  },
+  {
+    id: 12,
+    title: "BBQ burger",
+    image: "https://images.unsplash.com/photo-1572802419224-296b0aeee0d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1415&q=80",
+    description: "caramelized onions, cheddar, smokey bacon, BBQ sauce",
+    category: "burger"
+  },
+  {
+    id: 13,
+    title: "Chili Cheeseburger",
+    image: "https://images.unsplash.com/photo-1547584370-2cc98b8b8dc8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
+    description: "jalapeños, chipotle sauce, cheese, iceberg salad, tomato, dill pickles, red onions, mayo",
+    category: "burger"
+  },
 ];
